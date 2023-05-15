851ccfd4dbac49724634c8802312eebc69aca423
asikurr <asikurrahman7@gmail.com>
2023-05-15 16:21:05 +0600

    update app and added different type of model.

--- shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,3 +1,4 @@
+import { Recipe } from './../recipe.model';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is very good recipe', 
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
+    new Recipe('A Test Recipe2', 'This is very good recipe', 
+    'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+ 
+    ];
+    
+  constructor(){console.log(this.recipes);}
 }
